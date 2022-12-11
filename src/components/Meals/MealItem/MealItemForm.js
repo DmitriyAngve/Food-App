c3719d5e64eae94338ea4d5f6d95512c67c4e065
Dmitriy Angve <kirirch129@gmail.com>
2022-12-11 08:42:16 +0300

    #

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,7 +1,23 @@
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
+import { useRef } from "react";
+
+// ~CART REDUCER~
+// HELLO, I'm FROM CartProvider.js
+// Here wanna nadle the form submission, then "+ Add" button is clicked and item to the cart because that's what this form is there for ("<form className={classes.form}>")
+// STEP 2:
+// 2.1 Add "submitHandler" function in component "MealItemForm", where we get an "event" object which is sent as an argument automatically when I call this function (submitHandler) upon a built-in "event"
+// 2.2 Call "submitHandler" in form with "onSubmit" event (onSubmit={submitHandler})
+// 2.3 add "event.preventDefault()" - to make sure that, that browser default of reloading the page is prevented
+// 2.4 Then wanna extract the entered amount. Use "refs" for that - the alternative would be two way binding by manageing some state. Add import.
+// 2.5 Add "ref={}" prop to an Input.
+// 2.6 Add useRef in "MealItemForm" component ("amountInputRef") and pass into "ref={}", but "Input" this is a custom component the ref prop doesn't work at least not out of the box. For work do this:
+// 2.6.1 Go to the component where I wanna receive refs (Input.js)
+// ~CART REDUCER~
 
 const MealItemForm = (props) => {
+  const amountInputRef = useRef();
+
   // input here be used in multiple parts of the application (reusable pre-styled utility components like input/buttons in bigger application)
 
   // <Input label= "Amount"/> - label prop because I'm expecting a lebal prop form Input.js with some text inside of it
@@ -11,9 +27,15 @@ const MealItemForm = (props) => {
   // defaultValue - initial value. All this props is default props, which I can add to any input element. These a not props which we need to interpret and handle, instead we just distribute them onto the input through this syntax {...props.input} (from Input.js) and then they automatically start making sense, because these are all built-in props for input elements
 
   // "id: "amount_" + props.id" - make sure that the id props is passed correctly to <MealItemForm /> when that component is being used (i.e. inside of the MealItem component)
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount_" + props.id,
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,3 +1,4 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
 //~ CONTEXT~
@@ -11,14 +12,67 @@ import CartContext from "./cart-context";
 //~ CONTEXT~
 //
 
-const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
+// ~CART REDUCER~
+// Let's adding Cart Items. Here I manage put cart data. Than "addItemToCartHandler" is called I get the item that should be added to the cart,and then I wanna check if that item is already part of the cart (if I already have "item" in the cart) -> I wanna just update the existing item, if it's not the case, I wanna add a new item
+// Let's manange state of "CartProvider" component
+// LETS START: STEP 1
+// 1.1 Outside of this component function add my "cartReducer" function (outside because this function won't need anything in component function). And shouldn't be recreated all the time when the component is reevaluated
+// 1.2 Add arguments in "cartReducer" -> state object (last state snapshot of the state managed by the reducer) and action (the action is dispatched ne you later in your code)
+// 1.3 "return" in function -> returns a new state snapshot
+// 1.4 create another const "defaultCartState" -> it's an object, where i say that I have no items, aand "totalAmount" is zero
+// 1.5 And this "defaultCartState" I return in "cartReducer"
+// 1.6 Call useReducer at the top of "CartProvider" component with first argument "cartReducer" (don't executed, just point, react himself will execute) and initial state is "defaultCartState" (re-using this constant)
+// 1.7 Remember! useReducer returns an array with two elements -> use array destructuring and store them in separate constants. The first element of that array is always my state snapshot. Second element is function which allows to dispatch an action to the reducer (both customs names)
+// 1.8 After that a change my "hardcodded" my "cartContext" object "items: cartState.items" - because we're now managing the items with state and the same "totalAmount"
+// 1.9 Now I can start dispatching actions -> wanna start with adding items to the cart in "addItemToCartHandler" ("dispatchCartAction" - added in function) It's totally up to me what an action is. It could be a number or a text, but typicaly it's an object which has some property which allows you to identify that action inside of reducer function ("cartReducer"). "{type: "ADD"}" - convention
+// 1.9.1 Now wanna forward the "item" (arg) as part of the action. Second property -> named item (custom) and point at my "item"(arg). So that I'm forwarding the item which I expect to get here on this function to my reducer. DO same in "removeItemToCartHandler".
+// 1.10 Now go to the "cartReducer" and start adding logic for adding a cart item here.
+// 1.10.1 Adding "check if" the action.type is equal to ADD (check ADD or REMOVE which type of action it was triggered)
+// 1.10.2 Update my cart items. Add our "item" as a new element in that array and manage the amount on a per meal basis (update the "totalAmount" for all agregated cart items). Create an array "updatedItems" (items in the current state snapshot, which we get as a first argument in the reducer by React) and call concat(actio.item) (cuz return new array) Important: update state in an immutable way."concat(actio.item)" - which I getting -> all the data I need on that "action.item", I expect to have all of that on the "item", which reaches this "addItemToCartHandler"
+// 1.10.3. Now I get the "updatedItems", create "updatedTotalAmount" - > it should simply be the old totalAmount ("state.totalAmount" - on the old state snapshot). Multiply two action... I know by how much our totalAmount needs to change
+// 1.10.4 Then return new state snapshot were set items equals to updateItems and totalAmount same
+// FINISH logic: but now i need to make sure as NEXT STEP "addItemToCartHandler" is being called from some components in my application with appropriate data
+// Make sure that this "addItemToCartHandler" is being called, for that I need to go to the place where we wanna call "addItem" on our context object
 
-  const removeItemToCartHandler = (id) => {};
+// GO TO MealItemForm.js
+
+// ~CART REDUCER~
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat();
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return;
+};
+
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD" });
+  };
+
+  const removeItemToCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.items,
     addItem: addItemToCartHandler,
     removeItem: removeItemToCartHandler,
   };
