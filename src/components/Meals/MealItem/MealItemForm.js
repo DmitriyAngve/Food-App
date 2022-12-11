291bc1c2f549841335022d2b2008cf7fe6dafbe7
Dmitriy Angve <kirirch129@gmail.com>
2022-12-11 09:24:29 +0300

    #

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,22 +1,35 @@
+import { useRef, useState } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
-import { useRef } from "react";
 
-// ~CART REDUCER~
+// ~CART REDUCER ~ USEREF
 // HELLO, I'm FROM CartProvider.js
 // Here wanna nadle the form submission, then "+ Add" button is clicked and item to the cart because that's what this form is there for ("<form className={classes.form}>")
 // STEP 2:
-// 2.1 Add "submitHandler" function in component "MealItemForm", where we get an "event" object which is sent as an argument automatically when I call this function (submitHandler) upon a built-in "event"
+// 2.1 Add "submitHandler" function in component "MealItemForm", where we get an "event" object which is sent as an parameter automatically when I call this function (submitHandler) upon a built-in "event"
 // 2.2 Call "submitHandler" in form with "onSubmit" event (onSubmit={submitHandler})
 // 2.3 add "event.preventDefault()" - to make sure that, that browser default of reloading the page is prevented
 // 2.4 Then wanna extract the entered amount. Use "refs" for that - the alternative would be two way binding by manageing some state. Add import.
 // 2.5 Add "ref={}" prop to an Input.
 // 2.6 Add useRef in "MealItemForm" component ("amountInputRef") and pass into "ref={}", but "Input" this is a custom component the ref prop doesn't work at least not out of the box. For work do this:
-// 2.6.1 Go to the component where I wanna receive refs (Input.js)
-// ~CART REDUCER~
+// 2.6.1 Go to the component where I wanna receive refs (Input.js) and make sure you import React in there from React and ->
+// 2.6.2 And wrap Input component function with "React.forwardRef" - after this component function is now our argument to forwardRef
+// 2.6.3 Then add ref like React.forwardRe ("React.forwardRef(props, ref)"), which can now be set through the red prop on your Component as a second paramater
+// 2.6.4 Now use it (ref) inside of Component function to forward that ref to that input "<input ref={ref}..." after that ref is being set on "Input" component in MealItemForm.js
+// 2.7 Thats allows to read that entered value in "submitHandler". Add const "enteredAmount" by accesseing "amountInputRef.current.value". Its always stored current for refs created with useRef.value cuz "amountInputRef.current" will point at the input element which is stored in that ref in the end ("ref={amountInputRef}") which holds the currently ENTERED value. That value is always a string
+//2.8 Convert into a string ("enteredAmountNumber")
+// 2.9 Add "ifcheck" for validation where we check the "enteredAmount" as text, trimmed white space and length (m.b. empty) or 1<"enteredAmountNumber">5
+// 2.10 If this validation is checked I wanna return and not continue with this function (add return for fast stop) "submitHandler"
+// 2.11 Output error message below input and button and therefore I'll manage some state - > use useState -> it was simply control whether this form is valid or not and I'll set this to true initially and extract two values (state snapshot and the state updating function)
+// 2.12 And if I make it into this "ifcheck" in "submitHandler" -> the entered input was not valid -> ADD "setAmountIsValid(false)" and if "amountIsValid" is false, then I wanna output an error message
+// 2.13 Add below the "button" -> " {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}" - this is state and conditional rendering again
+// 2.14 I wanna execute my context methods to add a cart item, but not in this component. In this component I will call a function which I expect to get on props "onAddToCart(enteredAmountNumber)", cuz the cart item which I wanna add needs more data then just the entered amount. On this MealItemForm.js I only have that amount, I don't have the ID or NAME or a PRICE of that "item", that why I'm not calling the context method here, just calling some other function "onAddToCart" which I expect to get through props to parse the "enteredAmountNumber" and validated to that "onAddToCart" function.
+// 2.15 GO TO MealItem.js!!!
+// ~CART REDUCER ~ USEREF
 
 const MealItemForm = (props) => {
   const amountInputRef = useRef();
+  const [amountIsValid, setAmountIsValid] = useState(true);
 
   // input here be used in multiple parts of the application (reusable pre-styled utility components like input/buttons in bigger application)
 
@@ -30,6 +43,20 @@ const MealItemForm = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
   };
 
   return (
@@ -47,6 +74,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
     </form>
   );
 };
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,15 +1,16 @@
 import classes from "./Input.module.css";
+import React from "react";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   // "<label>{props.label}</label>" - label text should be configurable (label prop with label text and input prop which then itself holds an object with configuration data for the input, like "id")
 
   // {...props.input} trick to pass all other configuration data I might've gotten for this input to this input element here as props. This ensures that all the key value pairs in input object which we receive on props input are added as props to input (if we receive {type: "text"}, with this trick would make sure that "type="text" "is being added )
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}> {props.label}</label>
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
     </div>
   );
-};
+});
 
 export default Input;
